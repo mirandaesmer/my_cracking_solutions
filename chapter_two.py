59a95bf4c75b3653b941221b2b80875fc3133a2f
Miranda M <miranda.esmer.50@gmail.com>
2024-03-25 12:59:39 -0600

    add problems 2.1 2.2 2.6 and tests

---
+++ chapter_two.py
@@ -0,0 +1,59 @@
+from typing import Optional
+
+from linked_list_node import LinkedListNode
+
+
+class ChapterTwo:
+    def problem1(self, root: LinkedListNode) -> Optional[LinkedListNode]:
+        # Write code to remove duplicates from an unsorted linked list.
+        if root is None or root.data is None:
+            return None
+        
+        duplicates_map = {root.data: True}
+        node = root
+        while node.next is not None:
+            
+            # Remove duplicate if found
+            if node.next.data in duplicates_map:
+                node.next = node.next.next
+            else:
+                duplicates_map[node.next.data] = True
+            
+            node = node.next
+        return root
+    
+    def problem2(
+            self,
+            root: LinkedListNode,
+            k: int
+    ) -> Optional[LinkedListNode]:
+        # Implement an algorithm to find the kth to last element of a singly
+        # linked list.
+        if root is None or root.data is None:
+            return None
+        
+        kth_back = root
+        node = root
+        for _ in range(0, k):
+            if node.next is None:  # Less than k nodes
+                return None
+            node = node.next
+        
+        while True:
+            if node.next is None:
+                return kth_back
+            node = node.next
+            kth_back = kth_back.next
+    
+    def problem6(self, root: LinkedListNode) -> bool:
+        # Implement a function to check if a linked list is a palindrome.
+        if root is None or root.data is None:
+            return False
+        
+        node = root
+        data_list = [root.data]
+        while node.next is not None:
+            data_list.append(node.next.data)
+            node = node.next
+        
+        return data_list == data_list[::-1]
---
+++ chapter_two_tests.py
@@ -0,0 +1,54 @@
+from unittest import TestCase
+
+from chapter_two import ChapterTwo
+from linked_list_node import LinkedListNode
+
+
+class ChapterTwoTests(TestCase):
+    def setUp(self):
+        self.ch2 = ChapterTwo()
+    
+    def test_problem1(self) -> None:
+        root = LinkedListNode()
+        
+        arr = [3, 1, 5, 7, 1, 9, 5, 4]
+        for i in arr:
+            root.insert(i)
+        _ = self.ch2.problem1(root)
+        
+        data_list = [root.data]
+        node = root
+        while node.next is not None:
+            data_list.append(node.next.data)
+            node = node.next
+            
+        self.assertEqual(data_list, [3, 1, 5, 7, 9, 4])
+    
+    def test_problem2(self) -> None:
+        root = LinkedListNode()
+        for i in range(0, 10):
+            root.insert(i)
+        
+        node = self.ch2.problem2(root, 2)
+        self.assertEqual(node.data, 7)
+        
+        node = self.ch2.problem2(root, 5)
+        self.assertEqual(node.data, 4)
+        
+        node = self.ch2.problem2(root, 11)
+        self.assertIsNone(node)
+    
+    def test_problem6(self) -> None:
+        non_palin_arr = [1, 5, 7, 7, 4, 1, 5]
+        palin_arr = [1, 4, 8, 3, 8, 4, 1]
+        non_palin_root = LinkedListNode()
+        palin_root = LinkedListNode()
+        
+        for i in non_palin_arr:
+            non_palin_root.insert(i)
+        
+        for i in palin_arr:
+            palin_root.insert(i)
+            
+        self.assertFalse(self.ch2.problem6(non_palin_root))
+        self.assertTrue(self.ch2.problem6(palin_root))
