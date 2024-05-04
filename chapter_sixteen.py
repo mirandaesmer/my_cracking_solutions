710a147dfe204ac5732153d75f2b225ef099edf6
Miranda M <miranda.esmer.50@gmail.com>
2024-05-04 12:44:55 -0600

    solved problem 16.4 and complexity comment

--- chapter_sixteen.py
+++ chapter_sixteen.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, List, Optional
 
 
 class ChapterSixteen:
@@ -17,3 +17,28 @@ class ChapterSixteen:
                 num_a += 1
                 num_b -= 1
         return num_a, num_b
+    
+    def problem4(self, game: List[List[str]]) -> Optional[str]:
+        # Design an algorithm to figure out if someone has won a game of
+        # tic-tac-toe.
+        
+        # Complexity: O ( 9k ) where k is a constant at most the size of the
+        # gameboard, worst case scenario is still constant time
+        # Assuming board is formatted correctly
+        
+        # Rows
+        for row in game:
+            if row[0] == row[1] == row[2]:
+                return row[0]
+        
+        # Diagonals
+        if game[0][0] == game[1][1] == game[2][2]:
+            return game[0][0]
+        if game[0][2] == game[1][1] == game[2][0]:
+            return game[0][0]
+        
+        # Columns
+        for i in range(0, 3):
+            if game[i][0] == game[i][1] == game[i][2]:
+                return game[i][0]
+        return None
