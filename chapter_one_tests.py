53961d26d00ac4c30ca847f9b0b8749474d905e9
Miranda M <miranda.esmer.50@gmail.com>
2024-05-04 11:57:05 -0600

    add tests for problem 1.8

--- chapter_one_tests.py
+++ chapter_one_tests.py
@@ -52,7 +52,38 @@ class ChapterOneTests(TestCase):
             self.ch1.problem6('aabcccccaaa'),
             'a2b1c5a3'
         )
+    
+    def test_problem8(self):
+        base_case1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+        base_case2 = [[5, 1], [1, 8]]
         
+        self.assertEqual(self.ch1.problem8(base_case1), base_case1)
+        self.assertEqual(self.ch1.problem8(base_case2), base_case2)
+        
+        test_matrix = [
+            [1, 2, 3],
+            [4, 0, 7],
+            [5, 6, 3],
+        ]
+        exp_matrix = [
+            [1, 0, 3],
+            [0, 0, 0],
+            [5, 0, 3],
+        ]
+        self.assertEqual(self.ch1.problem8(test_matrix), exp_matrix)
+        
+        test_matrix = [
+            [0, 2, 3],
+            [4, 9, 0],
+            [5, 6, 3],
+        ]
+        exp_matrix = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 6, 0],
+        ]
+        self.assertEqual(self.ch1.problem8(test_matrix), exp_matrix)
+    
     def test_problem9(self):
         self.assertTrue(self.ch1.problem9('waterbottle', 'erbottlewat'))
         self.assertTrue(self.ch1.problem9('water', 'terwa'))
