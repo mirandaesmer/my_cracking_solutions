b55cb0e605a813dcbea0809db0fd7bbd59398a49
Miranda M <miranda.esmer.50@gmail.com>
2024-05-16 10:29:11 -0600

    add problem 8.3 and complexity comment

--- chapter_eight.py
+++ chapter_eight.py
@@ -1,4 +1,4 @@
-from typing import Set, List, Dict, Tuple
+from typing import Set, List, Dict, Tuple, Optional
 
 from image_array import ImageArray
 
@@ -26,6 +26,21 @@ class ChapterEight:
         ways_map = {0: 1, 1: 1}  # trivial cases
         return self._memo_step_count(n, ways_map)
     
+    def problem3(self, array: List[int]) -> Optional[int]:
+        # A magic index in an array A[0... n-1] is defined to be an index such
+        # that A[i] = i. Given a sorted array of distinct integers, write a
+        # method to find a magic index, if one exists, in array A.
+        
+        # Complexity: O( n ) at worst. Since array is made up of distinct
+        # sorted integers, we can return whenever the value is greater than the
+        # index.
+        for i in range(len(array)):
+            if array[i] > i:
+                return None
+            if array[i] == i:
+                return i
+        return None
+
     def problem4(self, elements: Set[int]) -> List[Set[int]]:
         # Write a method to return all subsets of a set.
         
